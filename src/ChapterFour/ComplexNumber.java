75d5f84f6264775f49a948bcaa775a0822568c40
Dai Pinshuang <Pinshuang_dai@163.com>
2020-01-09 12:17:19 +0800

    Intellij test

---
+++ src/ChapterFour/ComplexNumber.java
@@ -0,0 +1,19 @@
+package ChapterFour;
+
+public class ComplexNumber {
+    public static void main(String[] args){
+        String result = realAndImagine(1,2,3,4);
+        System.out.println(result);
+    }
+
+    public static String realAndImagine(int a,int b,int c,int d){
+        int temp1 = a*c;
+        int temp2 = b*d;
+        int P1 = temp1 - temp2;
+        int P2 = (a+b)*(c+d);
+        int P3 = temp1 + temp2;
+        int real = P1;
+        int imagine= P2 - P3;
+        return "The real is: "+real+" and the imagine is: "+imagine;
+    }
+}
